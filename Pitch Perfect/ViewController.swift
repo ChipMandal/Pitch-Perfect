ae193e4fd78f3723c5b6f06512ede90960bf88e9
cmandal <chitradip.mandal@gmail.com>
2015-03-17 00:30:05 -0700

    Lesson 2 working

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordPress(sender: UIButton, forEvent event: UIEvent) {
+        recordingLabel.hidden = false;
+    }
 
+    @IBAction func stopPress(sender: UIButton) {
+        recordingLabel.hidden = true;
+    }
 }
 
