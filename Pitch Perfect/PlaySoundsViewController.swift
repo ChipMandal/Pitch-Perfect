9ebfeb58cbe7f71ce9fe38990f9b130fd5d6475b
Chitradip Mandal <chitradip@chitradip.com>
2015-03-22 16:34:45 -0700

    Snail button

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
 
@@ -22,6 +23,9 @@ class PlaySoundsViewController: UIViewController {
     }
     
 
+    @IBAction func snailButton(sender: UIButton) {        
+        
+    }
     /*
     // MARK: - Navigation
 
