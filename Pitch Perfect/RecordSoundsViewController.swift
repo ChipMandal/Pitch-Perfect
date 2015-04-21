a2e45d275ebd5471f79857dea8faa02cb1d4c386
cmandal <chitradip.mandal@gmail.com>
2015-04-21 14:51:40 -0700

    Pause and Resume

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 import AVFoundation
 
+enum States {
+    case ReadyToRecord
+    case Recording
+    case Paused
+}
+
 class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
 
     let STOP_RECORDING_SEGUE = "stopRecording"
@@ -16,18 +22,67 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var stopButton: UIButton!
 
+    @IBOutlet weak var resumeButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
     @IBOutlet weak var recordButton: UIButton!
     
     var audioRecorder:AVAudioRecorder!
+
+    func updateUIBasedOnState(state:States) {
+        switch (state) {
+        case .ReadyToRecord:
+            stopButton.hidden = true;
+           
+            recordButton.enabled = true;
+            recordButton.hidden = false;
+            
+            pauseButton.hidden = true;
+            resumeButton.hidden = true;
+            
+            recordingLabel.hidden = false;
+            recordingLabel.text = "Tap to Record"
+        case .Paused:
+            stopButton.hidden = false;
+            
+            recordButton.hidden = true;
+            
+            pauseButton.hidden = true;
+            
+            resumeButton.enabled = true;
+            resumeButton.hidden = false;
+            
+            recordingLabel.text = "Tap to Resume"
+        case .Recording:
+            stopButton.hidden = false;
+            
+            recordButton.hidden = true;
+            
+            pauseButton.enabled = true;
+            pauseButton.hidden = false;
+            resumeButton.hidden = true;
+            
+            recordingLabel.text = "Recording"
+            
+            
+        }
+    }
+    
+    @IBAction func resumePress(sender: AnyObject) {
+        updateUIBasedOnState(States.Recording)
+        audioRecorder.record()
+    }
+    
+    @IBAction func pausePress(sender: AnyObject) {
+        updateUIBasedOnState(States.Paused)
+        audioRecorder.pause()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     override func viewWillAppear(animated: Bool) {
-        stopButton.hidden = true;
-        recordButton.enabled = true;
-        recordingLabel.text = "Tap to Record"
+        updateUIBasedOnState(States.ReadyToRecord)
 
     }
 
@@ -36,11 +91,10 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    
     @IBAction func recordPress(sender: UIButton, forEvent event: UIEvent) {
-        recordButton.enabled = false;
-        recordingLabel.text = "Recording"
-        stopButton.hidden = false;
         
+        updateUIBasedOnState(States.Recording)
         
         let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
         
@@ -61,8 +115,9 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         audioRecorder.prepareToRecord()
         audioRecorder.record()
         
-        
     }
+    
+    
     func audioRecorderDidFinishRecording(recorder: AVAudioRecorder!, successfully flag: Bool) {
         var recordingData: RecordingData = RecordingData(fileName: recorder.url.lastPathComponent!, filePath: recorder.url)
         if ( flag ) {
